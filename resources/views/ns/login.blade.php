2cc92f5226306cb41a31bb4d239a7ed0ae93ca3a
Syfreia#7203 <muguelangeldavidperez@gmail.com>
2024-02-29 22:18:40 +0100

    terminado

--- resources/views/ns/login.blade.php
+++ resources/views/ns/login.blade.php
@@ -15,43 +15,25 @@
         @endphp
         <!-- Navegación -->
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <div class="max-w-screen-xl flex items-center justify-between mx-auto p-2">
+            <div class="max-w-screen-xl flex flex-col md:flex-row items-center justify-between mx-auto p-2">
                 <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="{{ url('img/Asset 4.png') }}" class="h-10" alt="NAw STALL Logo" />
                 </a>
 
-                <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <!-- Campo de búsqueda -->
-                    <div class="relative mx-5">
-                        <input type="text"
-                            class="bg-gray-200 border border-gray-300 rounded-md py-1 px-3 focus:outline-none focus:ring-2 focus:ring-gray-400"
-                            placeholder="Buscar...">
-                        <span
-                            class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-                            <!-- Icono de búsqueda -->
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor"
-                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2"
-                                    d="M21 21l-4.823-4.822M9 17a8 8 0 100-16 8 8 0 000 16z" />
-                            </svg>
-                        </span>
-                    </div>
-
-                    <a class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
+                <div class="flex flex-col md:flex-row items-center md:order-2 space-y-3 md:space-y-0 md:space-x-3 rtl:space-x-reverse">
+                    <a class="block  m-2 py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
                         aria-current="page" href="{{ route('ns.login') }}">{{ __('titles.profile') }}</a>
                 </div>
 
-                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"id="navbar-user">
-                    <ul
-                        class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <div class="items-center justify-between w-full md:w-auto md:order-1" id="navbar-user">
+                    <ul class="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="{{ route('ns.index') }}"
                                 class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ __('titles.home') }}</a>
                         </li>
                         <li>
                             <a href="{{ route('ns.news') }}"
-                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">News</a>
+                                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ __('titles.newsest') }}</a>
                         </li>
                         <li>
                             <a href="{{ route('ns.wiki') }}"
@@ -65,6 +47,7 @@
                 </div>
             </div>
         </nav>
+
 
         <div class="flex justify-center items-center h-screen bg-blue-200">
             <div class="bg-white p-8 rounded-lg shadow-md">
--- resources/views/ns/wiki.blade.php
+++ resources/views/ns/wiki.blade.php
@@ -13,28 +13,25 @@
         
         <!-- Navegación -->
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
-            <div class="max-w-screen-xl flex items-center justify-between mx-auto p-2">
+            <div class="max-w-screen-xl flex flex-col md:flex-row items-center justify-between mx-auto p-2">
                 <a href="" class="flex items-center space-x-3 rtl:space-x-reverse">
                     <img src="{{ url('img/Asset 4.png') }}" class="h-10" alt="NAw STALL Logo" />
                 </a>
 
-                <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <!-- Campo de búsqueda -->
-                    <div class="relative mx-5">
-                        <input type="text" class="bg-gray-200 border border-gray-300 rounded-md py-1 px-3 focus:outline-none focus:ring-2 focus:ring-gray-400" placeholder="Buscar...">
-                        <span class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-                            <!-- Icono de búsqueda -->
-                            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-4.823-4.822M9 17a8 8 0 100-16 8 8 0 000 16z"/>
-                            </svg>
-                        </span>
-                    </div>
+                <div class="flex flex-col md:flex-row items-center md:order-2 space-y-3 md:space-y-0 md:space-x-3 rtl:space-x-reverse">
+                    <button data-collapse-toggle="navbar-user" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-user" aria-expanded="false">
+                        <span class="sr-only">{{ __('buttons.mainmenu') }}</span>
+                        <svg class="w-5 h-5" aria-hidden="true" fill="none" viewBox="0 0 17 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" />
+                        </svg>
+                    </button>
 
                     @if (Auth::check()) <!-- Verificar si el usuario ha iniciado sesión -->
                     <button type="button" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
                         <span class="sr-only">{{ __('buttons.usermenu') }}</span>
                         <img class="w-8 h-8 rounded-full" src="{{ url('img/profilePic/'.Auth::user()->img) }}" alt="user photo">
                     </button>
+
                     <!-- Dropdown menu -->
                     <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600" id="user-dropdown">
                         <div class="px-4 py-3">
@@ -57,11 +54,10 @@
                     @else
                     <a class="navbar-brand" href="{{ route('ns.login') }}">{{ __('titles.profile') }}</a>
                     @endif
-
                 </div>
 
-                <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                    <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                <div class="items-center justify-between w-full md:w-auto md:order-1" id="navbar-user">
+                    <ul class="flex flex-col md:flex-row font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
                             <a href="{{ route('ns.index') }}" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">{{ __('titles.home') }}</a>
                         </li>
